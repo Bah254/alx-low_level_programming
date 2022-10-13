751a27e1457127cbff80e64029305fa41777c1a0
Barua Masira <baruamasira@gmail.com>
2022-10-13 09:56:19 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
-#ifndef FUNCTIONS_POINTERS_H
-#define FUNCTIONS_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void _putchar(char *c)
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /*FUNCTIONS_POINTERS_H*/
+#endif /*FUNCTION_POINTERS_H*/
