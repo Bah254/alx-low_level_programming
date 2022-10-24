1f71a553915b2fd87300d76dc6e3775d1c026c99
Barua Masira <baruamasira@gmail.com>
2022-10-24 10:21:21 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,15 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_list - frees a list
+ * free_list - Frees a list_t list.
  * @head: points to the beginnin of list
  * Return: void
  */
 void free_list(list_t *head)
 {
 	list_t *phree;
+
 	while (head)
 	{
 		phree = head;
