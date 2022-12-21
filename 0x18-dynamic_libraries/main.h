4ba639e6e6735adc67a2a9a1af4eb12026fb8e59
Barua Masira <baruamasira@gmail.com>
2022-12-20 21:56:24 -0800

    Figuring it out

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,11 @@
-fndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int mul(int, int);
+int mod(int, int);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +27,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
